847319bfd54481ede4beb3ccf7ab993993c9683b
TheGam1ngWizard <ariwizard@gmail.com>
2022-10-31 22:58:21 -0400

    Fixed formatting (self. notation required), end states (cont., Win, and Draw)

--- core.py
+++ core.py
@@ -21,46 +21,46 @@ class TicTacToe:
         if self.tic_tac_board[int((block_num - 1)/3)][(block_num - 1)%3] == ' ':
             self.tic_tac_board[int((block_num - 1)/3)][(block_num - 1)%3] = player
         else:
-            block_num = int(input(f"Tile note empty, chose again: "))
+            block_num = int(input(f"Tile is not empty, chose again: "))
             self.play_move(player, block_num)
 
 
-    def current_board(self, tic_tac_board):
+    def current_board(self):
+        # Check horizontals
+        if (self.tic_tac_board[0][0] == self.tic_tac_board[0][1] and self.tic_tac_board[0][1] == self.tic_tac_board[0][2] and self.tic_tac_board[0][0] != ' '):
+            return "Done"
+        if (self.tic_tac_board[1][0] == self.tic_tac_board[1][1] and self.tic_tac_board[1][1] == self.tic_tac_board[1][2] and self.tic_tac_board[1][0] != ' '):
+            return "Done"
+        if (self.tic_tac_board[2][0] == self.tic_tac_board[2][1] and self.tic_tac_board[2][1] == self.tic_tac_board[2][2] and self.tic_tac_board[2][0] != ' '):
+            return "Done"
 
+        # Check verticals
+        if (self.tic_tac_board[0][0] == self.tic_tac_board[1][0] and self.tic_tac_board[1][0] == self.tic_tac_board[2][0] and self.tic_tac_board[0][0] != ' '):
+            return "Done"
+        if (self.tic_tac_board[0][1] == self.tic_tac_board[1][1] and self.tic_tac_board[1][1] == self.tic_tac_board[2][1] and self.tic_tac_board[0][1] != ' '):
+            return "Done"
+        if (self.tic_tac_board[0][2] == self.tic_tac_board[1][2] and self.tic_tac_board[1][2] == self.tic_tac_board[2][2] and self.tic_tac_board[0][2] != ' '):
+            return "Done"
+
+        # Check diagonals
+        if (self.tic_tac_board[0][0] == self.tic_tac_board[1][1] and self.tic_tac_board[1][1] == self.tic_tac_board[2][2] and self.tic_tac_board[0][0] != ' '):
+            return "Done"
+        if (self.tic_tac_board[2][0] == self.tic_tac_board[1][1] and self.tic_tac_board[1][1] == self.tic_tac_board[0][2] and self.tic_tac_board[2][0] != ' '):
+            return "Done"
+
+        # Check Draw State
         should_draw = 0
         for row in range(3):
             for col in range(3):
                 if self.tic_tac_board[row][col] == ' ':
                     should_draw = 1
         if should_draw == 0:
-            return None, "Draw"
+            return "Draw"
 
-        # Check horizontals
-        if (self.tic_tac_board[0][0] == self.tic_tac_board[0][1] and self.tic_tac_board[0][1] == self.tic_tac_board[0][2] and self.tic_tac_board[0][0] != ' '):
-            return tic_tac_board[0][0], "Done"
-        if (self.tic_tac_board[1][0] == self.tic_tac_board[1][1] and tic_tac_board[1][1] == self.tic_tac_board[1][2] and self.tic_tac_board[1][0] != ' '):
-            return self.tic_tac_board[1][0], "Done"
-        if (self.tic_tac_board[2][0] == self.tic_tac_board[2][1] and self.tic_tac_board[2][1] == self.tic_tac_board[2][2] and self.tic_tac_board[2][0] != ' '):
-            return self.tic_tac_board[2][0], "Done"
-
-        # Check verticals
-        if (self.tic_tac_board[0][0] == self.tic_tac_board[1][0] and tic_tac_board[1][0] == self.tic_tac_board[2][0] and self.tic_tac_board[0][0] != ' '):
-            return tic_tac_board[0][0], "Done"
-        if (self.tic_tac_board[0][1] == self.tic_tac_board[1][1] and self.tic_tac_board[1][1] == self.tic_tac_board[2][1] and self.tic_tac_board[0][1] != ' '):
-            return self.tic_tac_board[0][1], "Done"
-        if (self.tic_tac_board[0][2] == self.tic_tac_board[1][2] and self.tic_tac_board[1][2] == self.tic_tac_board[2][2] and self.tic_tac_board[0][2] != ' '):
-            return self.tic_tac_board[0][2], "Done"
-
-        # Check diagonals
-        if (self.tic_tac_board[0][0] == self.tic_tac_board[1][1] and self.tic_tac_board[1][1] == self.tic_tac_board[2][2] and self.tic_tac_board[0][0] != ' '):
-            return self.tic_tac_board[1][1], "Done"
-        if (self.tic_tac_board[2][0] == self.tic_tac_board[1][1] and self.tic_tac_board[1][1] == self.tic_tac_board[0][2] and self.tic_tac_board[2][0] != ' '):
-            return self.tic_tac_board[1][1], "Done"
-
-        return None, "Not Done"
+        return "Not Done"
 
 
-    def print_board(self, tic_tac_board):
+    def print_board(self):
         print('----------------')
         print('| ' + str(self.tic_tac_board[0][0]) + ' || ' + str(
             self.tic_tac_board[0][1]) + ' || ' + str(self.tic_tac_board[0][2]) + ' |')
@@ -79,28 +79,52 @@ class TicTacToe:
 
 
     def run_game(self):
+
+        # Startup
         current_state = "Not Done"
         print(f"New Game!")
-        self.print_board(self.tic_tac_board)
+
+        # Show Blank Gameboard
+        self.print_board()
+
+        # Choose Starting Player & Set gamestate to 'not won'
         player_choice = input("Choose which player goes first - X or O: ")
         winner = None
+        
+        # Init Playerstate
         if player_choice == 'X' or player_choice == 'x':
             current_player_idx = 0
-        else:
+        elif player_choice == 'O' or player_choice == 'o':
             current_player_idx = 1
+        else: 
+            print(f"That is not a valid choice, please try again.")
+            self.run_game()
+
+        # Rungame
         while current_state == "Not Done":
+            # Choose move
             block_choice = int(input(
                 str(self.players[current_player_idx]) + "'s Turn! Choose where to place (1 to 9): "))
             self.play_move(self.players[current_player_idx], block_choice)
-            self.print_board(self.tic_tac_board)
-            winner, current_state = self.current_board(self.tic_tac_board)
-            if winner != None:
-                print(str(winner) + " won!")
-            else:
+            self.print_board()
+            current_state = self.current_board()
+            
+            # debug
+            print(str(current_state))
+            
+            # These provide the exact same results
+            # print(self.players[current_player_idx])
+            # print(str(self.players[current_player_idx]))
+            
+            # TODO: Fix 'draw' state/end state problem
+            if current_state == "Not Done":
                 current_player_idx = (current_player_idx + 1) % 2
-
-            if current_state == "Draw":
+            elif current_state == "Done":
+                print(self.players[current_player_idx] + " won!")
+            else:
                 print("Draw!")
+                
+                
 
 
 # Running the project
